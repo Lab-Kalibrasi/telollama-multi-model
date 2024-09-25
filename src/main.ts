488a6ff605a3096dc3714c7ebb91b3000c6e8dd9
Perdana Hadi <ceroberoz@gmail.com>
2024-09-26 00:46:44 +0700

    add advance emotion system

--- src/main.ts
+++ src/main.ts
@@ -70,19 +70,23 @@ const googleAI = new GoogleGenerativeAI(Deno.env.get("GOOGLE_AI_API_KEY") || "")
 
 const safetySettings = [
   {
-    category: HarmCategory.HARASSMENT,
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
     threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
   },
   {
-    category: HarmCategory.HATE_SPEECH,
+    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
     threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
   },
   {
-    category: HarmCategory.SEXUALLY_EXPLICIT,
+    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
     threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
   },
   {
-    category: HarmCategory.DANGEROUS_CONTENT,
+    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_CIVIC_INTEGRITY,
     threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
   },
 ];
